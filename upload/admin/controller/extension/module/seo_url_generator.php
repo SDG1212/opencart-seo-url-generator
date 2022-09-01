514f9f24497ee2cf273f1c8aca3b200717165952
SDG1212 <sokol1294@gmail.com>
2022-09-01 04:04:15 +0300

    Add Copyright Info

--- upload/admin/controller/extension/module/seo_url_generator.php
+++ upload/admin/controller/extension/module/seo_url_generator.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @author Dmytro Sokolenko <sokol1294@gmail.com>
+ * @license GNU General Public License, version 3. See LICENSE
+ */
 class ControllerExtensionModuleSeoUrlGenerator extends Controller {
 	private $error = array();
 
--- upload/admin/language/en-gb/extension/module/seo_url_generator.php
+++ upload/admin/language/en-gb/extension/module/seo_url_generator.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @author Dmytro Sokolenko <sokol1294@gmail.com>
+ * @license GNU General Public License, version 3. See LICENSE
+ */
 // Heading
 $_['heading_title']    = 'SEO URL Generator';
 
